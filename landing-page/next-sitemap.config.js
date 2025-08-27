78ec0e087aad2af65b31ba1f06a9ce3bf4ced6e6
Boiri Gourav - LFI <bgourav@linkfields.com>
2025-08-28 01:48:00 +0530

    minor

---
+++ landing-page/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://reflyx-ai.vercel.app/',  // <-- Replace with your domain
+  generateRobotsTxt: true,        // Generate robots.txt as well
+  sitemapSize: 7000,              // (optional) max URLs per sitemap
+};
